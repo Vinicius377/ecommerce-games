a79617807eba66150177308d60048bf16a1fbbaa
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-13 13:59:49 -0300

    global styles

---
+++ src/style/App.style.ts
@@ -0,0 +1,11 @@
+import style from "styled-components"
+
+export const Main = style.main`
+    width:85vw;
+    margin-top:30px
+`
+export const Container = style.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+`
