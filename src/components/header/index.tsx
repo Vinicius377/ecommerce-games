5f454f94feed0a5932391f1551a20448d8932026
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-14 15:45:10 -0300

    add search-input

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,19 +2,37 @@ import { Container } from "./style"
 import { NavLink } from "react-router-dom"
 import { useContext } from "react"
 import CartContext from "../../context/Cart-Context"
+import { useNavigate } from "react-router-dom"
 
 import acount_img from "../../assets/account_avatar.svg"
 import arrow_down from "../../assets/arrow-down-icon.svg"
+import cartIcon from "../../assets/cart_icon.svg"
+import searchIcon from "../../assets/search_icon.svg"
 
 function Header() {
   const { total } = useContext(CartContext)
+  const navigate = useNavigate()
   return (
     <Container>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/checkout">Checkout</NavLink>
-        <h1>R${total?.toFixed(2)}</h1>
+        <div
+          className="subtotal-container"
+          onClick={() => navigate("/checkout")}
+        >
+          <h2>R$ {total?.toFixed(2)}</h2>
+          <img src={cartIcon} alt="cart-icon" width={30} />
+        </div>
       </nav>
+      <div role="searchbox" className="search-box">
+        <input
+          type="text"
+          role="search"
+          placeholder="Pesquise algum jogo aqui!"
+        />
+        <img src={searchIcon} alt="search_icon" width={30} />
+      </div>
       <div className="account-container">
         <img src={arrow_down} alt="arrow_down" />
         <img src={acount_img} alt="account_img" width={40} />
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -4,7 +4,7 @@ export const Container = styled.header`
   background: #d2d7d8;
   width: 100vw;
   display: flex;
-  align-itens: center;
+  align-items: center;
   justify-content: space-between;
   padding-inline: 100px;
   a {
@@ -13,10 +13,44 @@ export const Container = styled.header`
     font-weight: bold;
     font-size: 1.2rem;
     text-decoration: none;
+    padding: 15px 10px;
+    :hover {
+      background: #c2c6c7;
+    }
   }
   nav {
-    padding: 20px 50px;
     display: flex;
+    align-items: center;
+  }
+  .active {
+    border-bottom: solid 3px #51e7f6;
+  }
+  .subtotal-container {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    gap: 5px;
+    transition: 0.2s;
+    :hover {
+      -webkit-text-stroke: 0.2px black;
+      color: #51e7f6;
+    }
+  }
+  .search-box {
+    flex: 1;
+    margin: 10px 50px;
+    display: flex;
+    img {
+      margin-inline: 15px;
+      cursor: pointer;
+    }
+    input {
+      width: 100%;
+      border: none;
+      outline: none;
+      padding: 5px;
+      font-size: 1.02rem;
+    }
   }
   .account-container {
     cursor: pointer;
@@ -29,6 +63,8 @@ export const Container = styled.header`
     display: flex;
   }
   .account-modal {
+    cursor: default;
+    border-radius: 5px;
     display: none;
     flex-direction: column;
     background: #ffffff;
@@ -38,6 +74,6 @@ export const Container = styled.header`
     position: absolute;
     left: 0;
     z-index: 100;
-    top: 60px;
+    top: 50px;
   }
 `
