230cdf7dd178e28342cf05c309599fc261c9a1ad
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-16 14:45:05 -0300

    add toast container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.min.css"
+
+import Header from "./components/header"
+import Checkout from "./pages/Checkout"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Login from "./pages/Login"
+import { Main, Container } from "./style/App.style"
 
 function App() {
   return (
-    <BrowserRouter>
-      asd
-      <Routes></Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </Main>
+      </BrowserRouter>
+      <ToastContainer />
+    </Container>
   )
 }
 
