97bb94d8d311e00ad565bf46f139d10448f06d97
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-16 21:37:11 -0300

    wrap contexts on app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Global } from "./Style.global"
+import { Global } from "./style/Style.global"
 import App from "./App"
+import { CartContextProvider } from "./context/Cart-Context"
+import { ProductsContextProvider } from "./context/Products-Context"
 
 ReactDOM.render(
   <React.StrictMode>
     <Global />
-    <App />
+    <ProductsContextProvider>
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
+    </ProductsContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
