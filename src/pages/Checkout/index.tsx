9f5bf7428c4850deb83ec616d5b26b8f8a79364d
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-17 00:25:21 -0300

    add checkout button

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -8,24 +8,29 @@ function Checkout() {
 
   return (
     <Container>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Preço</th>
-          <th>Score</th>
-          <th>Quantidade</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cartCheckout &&
-          cartCheckout.map(item => (
-            <ItemCheckout informations={item} key={item.id} />
-          ))}
-        <tr className="subtotal">
-          <th>SubTotal:</th>
-          <td>R$ {total?.toFixed(2)}</td>
-        </tr>
-      </tbody>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Preço unitário</th>
+            <th>Score</th>
+            <th>Quantidade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartCheckout &&
+            cartCheckout.map(item => (
+              <ItemCheckout informations={item} key={item.id} />
+            ))}
+          <tr className="subtotal">
+            <th>SubTotal:</th>
+            <td>R$ {total?.toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button className="button-buy" disabled={total === 0 ? true : false}>
+        Finalizar compra
+      </button>
     </Container>
   )
 }
--- src/pages/Checkout/style.ts
+++ src/pages/Checkout/style.ts
@@ -1,12 +1,37 @@
 import styled from "styled-components"
 
-export const Container = styled.table`
+export const Container = styled.section`
   width: 100%;
-  text-align: left;
-  gap: 10px;
-  color: #ffffff;
-  font-size: 1.15rem;
-  .subtotal {
-    font-size: 1.2rem;
+  table {
+    width: 100%;
+    text-align: left;
+    gap: 10px;
+    color: #ffffff;
+    font-size: 1.15rem;
+    .subtotal {
+      font-size: 1.2rem;
+    }
   }
+  .button-buy {
+    width: 100%;
+    cursor: pointer;
+    background-color: #ff965a;
+    border: solid 0.2px black;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top:10px ;
+    padding: 15px;
+    font-size: 1.2rem;
+    font-weight: 600;
+    :hover {
+      background-color: #ffffff;
+      border: solid 1px #ff965a;
+    }
+    :disabled{
+      background-color: #ffffff;
+      border: solid 1px #ff965a;
+
+    }
 `
