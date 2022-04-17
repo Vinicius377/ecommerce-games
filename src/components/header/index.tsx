4e0d14622ab129611238a511552c13754d6edd90
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-17 11:01:30 -0300

    add reponsive menu hamburguer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,11 @@
 import { NavLink, Link } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-import { Container } from "./style"
+import { Container, MenuHamb } from "./style"
 import CartContext from "../../context/Cart-Context"
 
-import acount_img from "../../assets/account_avatar.svg"
+import account_img from "../../assets/account_avatar.svg"
 import arrow_down from "../../assets/arrow-down-icon.svg"
 import cart_icon from "../../assets/cart_icon.svg"
 import search_icon from "../../assets/search_icon.svg"
@@ -13,8 +13,20 @@ import search_icon from "../../assets/search_icon.svg"
 function Header() {
   const { total } = useContext(CartContext)
   const navigate = useNavigate()
+  const [showMenu, setShowMenu] = useState(true)
+
+  const onNavigate = () => {
+    navigate("/checkout")
+  }
   return (
-    <Container>
+    <Container openMenu={showMenu}>
+      <div
+        className="menu-bar"
+        onClick={() => setShowMenu(prevState => !prevState)}
+      >
+        <MenuHamb active={showMenu} />
+      </div>
+
       <nav role="menu">
         <NavLink to="/" role="menuitem">
           Home
@@ -22,14 +34,12 @@ function Header() {
         <NavLink to="/checkout" role="menuitem">
           Checkout
         </NavLink>
-        <div
-          className="subtotal-container"
-          onClick={() => navigate("/checkout")}
-        >
+        <div className="subtotal-container" onClick={onNavigate}>
           <h2>R$ {total?.toFixed(2)}</h2>
           <img src={cart_icon} alt="icone de carrinho de compra" width={30} />
         </div>
       </nav>
+      <div></div>
       <div role="searchbox" className="search-box">
         <input
           type="text"
@@ -40,11 +50,11 @@ function Header() {
       </div>
       <div className="account-container">
         <img src={arrow_down} alt="flecha para baixo" />
-        <img src={acount_img} alt="imagem padrão de perfil" width={40} />
+        <img src={account_img} alt="imagem padrão de perfil" width={40} />
         <div className="account-modal">
           <Link to="/login">Fazer login </Link>
           <span>ou</span>
-          <Link to="/signin">Cadastrar</Link>
+          <Link to="/signin">Criar uma conta</Link>
         </div>
       </div>
     </Container>
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,150 +1,218 @@
 import styled from "styled-components"
 
-export const Container = styled.header`
-  background: #d2d7d8;
-  width: 100vw;
+interface ContainerProps {
+  openMenu: boolean
+}
+export const Container = styled.header(
+  ({ openMenu }: ContainerProps) => `
+background: #d2d7d8;
+width: 100vw;
+display: flex;
+align-items: center;
+padding-inline: 100px;
+
+align-items: center;
+justify-content: space-between;
+
+nav a {
+  color: black;
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-decoration: none;
+  padding: 15px 10px;
+  :hover {
+    backdrop-filter: brightness(0.9);
+  }
+}
+nav {
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  padding-inline: 100px;
-  nav a {
+}
+.active {
+  border-bottom: solid 3px #51e7f6;
+}
+.subtotal-container {
+  margin-inline: 15px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  gap: 5px;
+  transition: 0.2s;
+  :hover {
+    -webkit-text-stroke: 0.2px black;
+    color: #51e7f6;
+  }
+}
+.search-box {
+  flex: 1;
+  margin: 10px 50px;
+  display: flex;
+  img {
+    margin-inline: 15px;
+    cursor: pointer;
+  }
+  input {
+    width: 100%;
+    border: none;
+    outline: none;
+    padding: 5px;
+    font-size: 1.02rem;
+  }
+}
+.account-container {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  position: relative;
+  :hover .account-modal {
+    display: flex;
+  }
+}
+
+.account-modal {
+  cursor: default;
+  border-radius: 5px;
+  display: none;
+  flex-direction: column;
+  background: #ffffff;
+  border: solid 0.4px black;
+  position: absolute;
+  right: 0;
+  white-space: pre;
+  padding-block: 10px;
+  z-index: 100;
+  font-size: 1.2rem;
+  top: 40px;
+  span {
+    border-block: solid 0.5px rgba(0, 0, 0, 0.5);
+    text-align: center;
+    padding-block: 5px;
+    margin-inline: 5px;
+  }
+  a {
+    padding: 5px 20px;
     color: black;
-    font-weight: bold;
-    font-size: 1.2rem;
     text-decoration: none;
-    padding: 15px 10px;
+    font-size: 1.1rem;
+    text-align: center;
     :hover {
       backdrop-filter: brightness(0.9);
     }
   }
-  nav {
-    display: flex;
-    align-items: center;
+}
+.menu-bar {
+  cursor: pointer;
+  padding: 20px;
+  position: relative;
+  display: none;
+}
+
+/* Media querys */
+@media (max-width: 1050px) {
+  .search-box {
+    margin: 10px;
   }
-  .active {
-    border-bottom: solid 3px #51e7f6;
+   {
+    padding-inline: 50px;
   }
-  .subtotal-container {
-    margin-inline: 15px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    gap: 5px;
-    transition: 0.2s;
-    :hover {
-      -webkit-text-stroke: 0.2px black;
-      color: #51e7f6;
-    }
+}
+@media (max-width: 800px) {
+  nav a {
+    font-size: 1.3em;
+  }
+   {
+    padding-inline: 30px;
+    font-size: 0.85em;
+  }
+}
+@media (max-width: 720px) {
+   {
+    display: grid;
+    grid-template-columns: 2fr 1fr 1fr;
+    padding: 0 10px;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
   .search-box {
-    flex: 1;
-    margin: 10px 50px;
-    display: flex;
-    img {
-      margin-inline: 15px;
-      cursor: pointer;
-    }
+    order: 2;
+    width: 100%;
+    margin: 20px 0;
+    flex: 0;
+    grid-column: 1/4;
+    display: ${openMenu ? "flex" : "none"};
+
     input {
+      padding: 8px;
+    }
+  }
+  nav {
+    width: 100%;
+    order: 3;
+    height: 100vh;
+    grid-column: 1/4;
+    display:${openMenu ? "flex" : "none"};
+    flex-direction: column;
+    a {
+      text-align: center;
       width: 100%;
-      border: none;
-      outline: none;
-      padding: 5px;
-      font-size: 1.02rem;
     }
   }
   .account-container {
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    position: relative;
-    :hover .account-modal {
-      display: flex;
-    }
+    order: 1;
+    margin-right: 20px;
+    align-self: flex-end;
+    grid-column: 3/4;
+    justify-content: center;
   }
-
+  .subtotal-container {
+    order: 2;
+    grid-column: 2/3;
+  }
   .account-modal {
-    cursor: default;
-    border-radius: 5px;
-    display: none;
-    flex-direction: column;
-    background: #ffffff;
-    border: solid 0.4px black;
-    position: absolute;
-    left: 0;
-    padding-block: 10px;
-    z-index: 100;
-    top: 40px;
-    span {
-      border-block: solid 0.5px rgba(0, 0, 0, 0.5);
-      text-align: center;
-      padding-block: 5px;
-    }
-    a {
-      padding: 5px 20px;
-      color: black;
-      text-decoration: none;
-      font-size: 1.1rem;
-      text-align: center;
-      :hover {
-        backdrop-filter: brightness(0.9);
-      }
-    }
+    height: 100vh;
+    width: 100vw;
   }
+  .menu-bar {
+    display: block;
+  }
+}
+`
+)
 
-  /* Media querys */
-  @media (max-width: 1050px) {
-    .search-box {
-      margin: 10px;
-    }
-     {
-      padding-inline: 50px;
-    }
+interface MenuHamb {
+  active?: boolean
+}
+export const MenuHamb = styled.div.attrs(() => ({
+  role: "menubar",
+}))`
+  width: 35px;
+  height: 4px;
+  background: black;
+  position: relative;
+  ${(props: MenuHamb) => props.active && "transform: rotate(45deg);"}
+  transition: 0.3s;
+  :before {
+    ${(props: MenuHamb) => props.active && "opacity:0;"}
+    content: "";
+    position: absolute;
+    transition: 0.3s;
+    top: 8px;
+    display: block;
+    width: 35px;
+    height: 4px;
+    background: black;
   }
-  @media (max-width: 800px) {
-    nav a {
-      font-size: 1.3em;
-    }
-     {
-      padding-inline: 30px;
-      font-size: 0.85em;
-    }
+  :after {
+    content: "";
+    transition: 0.3s;
+    position: absolute;
+    bottom: 8px;
+    display: block;
+    ${(props: MenuHamb) =>
+      props.active && "transform: rotate(90deg) translatex(8px);"}
+    width: 35px;
+    height: 4px;
+    background: black;
   }
 `
-
-// interface MenuBar {
-//   active?: boolean
-// }
-// export const MenuBar = styled.div.attrs(() => ({
-//   role: "menubar",
-// }))`
-//   width: 35px;
-//   height: 4px;
-//   background: black;
-//   position: relative;
-//   ${(props: MenuBar) => props.active && "transform: rotate(45deg);"}
-//   transition: 0.7s;
-//   :before {
-//     ${(props: MenuBar) => props.active && "opacity:0;"}
-//     content: "";
-//     position: absolute;
-//     transition: 0.7s;
-//     top: 5px;
-//     display: block;
-//     width: 35px;
-//     height: 4px;
-//     background: black;
-//   }
-//   :after {
-//     content: "";
-//     transition: 0.7s;
-//     position: absolute;
-//     bottom: 5px;
-//     display: block;
-//     ${(props: MenuBar) =>
-//       props.active && "transform: rotate(90deg) translatex(5px);"}
-//     width: 35px;
-//     height: 4px;
-//     background: black;
-//   }
-// `
