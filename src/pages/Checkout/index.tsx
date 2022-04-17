adbd7d271fd826f54a2d40c539810f2f2d002d0a
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-16 21:35:39 -0300

    page checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,33 @@
+import ItemCheckout from "../../components/item-checkout"
+import { Container } from "./style"
+import { useContext } from "react"
+import CartContext from "../../context/Cart-Context"
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const { cartCheckout, total } = useContext(CartContext)
+
+  return (
+    <Container>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Score</th>
+          <th>Quantidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartCheckout &&
+          cartCheckout.map(item => (
+            <ItemCheckout informations={item} key={item.id} />
+          ))}
+        <tr className="subtotal">
+          <th>SubTotal:</th>
+          <td>R$ {total?.toFixed(2)}</td>
+        </tr>
+      </tbody>
+    </Container>
+  )
 }
 
-export default Checkout;
+export default Checkout
---
+++ src/pages/Checkout/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Container = styled.table`
+  width: 100%;
+  text-align: left;
+  gap: 10px;
+  color: #ffffff;
+  font-size: 1.15rem;
+  .subtotal {
+    font-size: 1.2rem;
+  }
+`
